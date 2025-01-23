435e1787d79a14e4ba9e5408e5c7cd9c33bb0fe2
Sruthi Mavuleti <mavuleti160@gmail.com>
2025-01-23 15:17:08 -0800

    Updated Url

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -4,7 +4,7 @@ import { Observable, throwError, catchError } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 //Declaring the api url that will provide data for the client app
-const apiUrl = 'https://smavuleti-moviebee-479d2e8d7a6f.herokuapp.com';
+const apiUrl = 'https://smavuleti-moviebee-479d2e8d7a6f.herokuapp.com/';
 @Injectable({
   providedIn: 'root'
 })
